8440fab773dd51680eb05939680e8e5d8ef352a8
visalakshi2001 <visalakshi2001@gmail.com>
2025-03-07 16:14:51 -0700

    adding graph branching

--- system.py
+++ system.py
@@ -86,8 +86,67 @@ def systemview():
             if pd.notna(assembly):
                 dot.edge(assembly, component, label="has component")
 
-    st.graphviz_chart(dot)
+    # st.graphviz_chart(dot)
+
+    systemdot = graphviz.Digraph(
+                comment='Hierarchy',  
+                graph_attr={"height":"1000", "width":"1000", "rankdir":"LR"},
+                strict=True
+            )
+    
+
+    completesystemsdf = pd.concat([subsystemdf, assembliesdf, componentsdf])
 
 
+    for index, row in completesystemsdf.iterrows():
+        soi = row["SOI"]
+        subsystem = row["Subsystem"]
+        
         
 
+        if pd.notna(soi) and soi not in dot.body:
+            systemdot.node(soi)
+
+        if pd.notna(subsystem):
+            if subsystem not in dot.body:
+                systemdot.node(subsystem)
+            if pd.notna(soi):
+                systemdot.edge(soi, subsystem, label="has subsystem") 
+        
+
+    subsystemopts = st.multiselect(label="Select one/multiple subsystem(s) to expand and  display", options=completesystemsdf["Subsystem"].unique())
+
+    if subsystemopts != []:
+        subsetdf = completesystemsdf[completesystemsdf["Subsystem"].isin(subsystemopts)]
+
+        for index, row in subsetdf.iterrows():
+            soi = row["SOI"]
+            subsystem = row["Subsystem"]
+            assembly = row["Assembly"]
+            component = row["Component"]
+
+
+            if pd.notna(soi) and soi not in systemdot.body:
+                systemdot.node(soi)
+
+            if pd.notna(subsystem):
+                if subsystem not in systemdot.body:
+                    systemdot.node(subsystem)
+                if pd.notna(soi):
+                    systemdot.edge(soi, subsystem, label="has subsystem")
+            
+            if pd.notna(assembly):
+                if assembly not in systemdot.body:
+                    systemdot.node(assembly)
+                if pd.notna(subsystem):
+                    systemdot.edge(subsystem, assembly, label="has assembly")
+
+            if pd.notna(component):
+                if component not in systemdot.body:
+                    systemdot.node(component)
+                if pd.notna(assembly):
+                    systemdot.edge(assembly, component, label="has component")
+
+    cols = st.columns(2)
+
+    st.graphviz_chart(systemdot, True)
